b9bd8b365049cf67ffefd3985ac35734825d8f39
Nicolas Andreoli <nicolas.andreoli@playtika.com>
2020-12-03 22:14:33 -0300

    first commit

---
+++ src/test/java/ar/com/porloschicos/cloudconfig/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.porloschicos.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
